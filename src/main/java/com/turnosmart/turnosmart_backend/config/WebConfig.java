bc054db3b951319557d78b49aafd30f75ab13650
Gean <GEAN10cuenta@gmail.com>
2026-06-29 22:18:15 -0500

    Ahora el abogado te aprueba y puedes subir PDF y el abogado puede ver eso :,)

--- src/main/java/com/turnosmart/turnosmart_backend/config/WebConfig.java
+++ src/main/java/com/turnosmart/turnosmart_backend/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.turnosmart.turnosmart_backend.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +16,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(securityInterceptor)
                 .addPathPatterns("/admin/**", "/abogado/**", "/cliente/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/tramites/**")
+                .addResourceLocations("file:uploads/tramites/");
     }
 }
